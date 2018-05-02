2cdcdeef5cc7f5404c803a7efeac5f012f6a8ca5
senin8 <malizziaaries@gmail.com>
2018-05-03 00:15:19 +0200

    Add project files.

---
+++ EjercicioConcatenar/Concatena.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioConcatenar
+{
+    public class Concatena
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+        //constructor
+        public Concatena(string nomb, string apel)
+        {
+            Nombre = nomb;
+            Apellido = apel;
+        }
+        //método devuelve un return 
+        public string Concatenar()
+        {
+            StringBuilder sb = new StringBuilder();
+            return sb.AppendFormat("{0} {1}",Nombre, Apellido).ToString();
+            
+        }
+
+    }
+}
+
+
+//(3.6). Crear un programa que contenga la clase ‘Concatena.cs’ con dos atributos 
+//“nombre” y “apellido”. Tendrá un constructor que recibirá esos dos parámetros y un 
+//    método “concatenar” que devolverá(return) el nombre correctamente concatenado al 
+//    apellido”. Crear, asi mismo, una clase program.cs cuyo Main pedirá el nombre y el 
+//        apellido del usuario e imprimirá por pantalla el nombre concatenado.
---
+++ EjercicioConcatenar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioConcatenar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dime tu nombre");
+            string n = Console.ReadLine();
+
+            Console.WriteLine("Dime tu apellido");
+            string a = Console.ReadLine();
+
+            Concatena resultado = new Concatena(n , a);
+            Console.WriteLine(resultado.Concatenar());
+
+            Console.Read();
+        }
+    }
+}
+//Crear, asi mismo, una clase program.cs cuyo Main pedirá el nombre y el
+//        apellido del usuario e imprimirá por pantalla el nombre concatenado.
